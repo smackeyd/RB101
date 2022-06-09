a37e895c93a462b2c267a05958ff8cb1e91a63b1
Sam MacDonald <89047082+smackeyd@users.noreply.github.com>
2022-06-09 13:41:22 -0400

    Delete lesson2_small_problems directory

--- lesson2_small_problems/assignment20_pass_by_reference.rb
+++
@@ -1,9 +0,0 @@
-def cap(str)
-  str.capitalize!   # does this affect the object outside the method?
-end
-
-name = "jim"
-cap(name)
-puts name           # => Jim
-
-# will NOT work if we used capitalize without b!
--- lesson2_small_problems/test.rb
+++
@@ -1,3 +0,0 @@
-p "hello".center(10)
-p "hello".center(20)
-p "hello".center(5)
